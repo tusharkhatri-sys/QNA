95dda378b88f8c3f1865eb58d65816dbc9d71ef6
tusharkhatri-sys <tusharkhatri002@gmail.com>
2026-05-27 12:44:32 +0530

    fix: show Details column header and report student live status on join

--- public/pages/quiz.js
+++ public/pages/quiz.js
@@ -151,6 +151,7 @@ function initPracticeMode() {
         currentQuiz = currentQuiz.slice(0, 50);
     }
     renderQuestion();
+    if (testData) reportLiveProgress();
 }
 
 function updateTimerDisplay() {
